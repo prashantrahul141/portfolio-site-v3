fa5e2373ea2de207ef6268cb1eff4702187ee4de
Prashant <prashantrahul141@protonmail.com>
2023-05-04 17:10:40 +0530

    add: ids to navigationOptions

--- utils/constantTSX.tsx
+++ utils/constantTSX.tsx
@@ -1,7 +1,7 @@
-const navigationOptions: Array<{ text: string; link: string }> = [
-  { text: 'Home', link: '/home/' },
-  { text: 'Blogs', link: '/home/blogs' },
-  { text: 'Gallery', link: '/home/gallery' },
-];
+const navigationOptions = [
+  { id: 0, text: 'Home', link: '/home/' },
+  { id: 1, text: 'Blogs', link: '/home/blogs' },
+  { id: 2, text: 'Gallery', link: '/home/gallery' },
+] as const;
 
 export { navigationOptions };
