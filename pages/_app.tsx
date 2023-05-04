7296064290b32edbc2f93f007a5f08ac43d071f0
Prashant <prashantrahul141@protonmail.com>
2023-05-04 15:03:24 +0530

    add: page animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <AnimatePresence mode='wait'>
+      <motion.div
+        key={router.route}
+        initial='initialState'
+        animate='animateState'
+        exit='exitState'
+        transition={{
+          duration: 0.75,
+          ease: 'easeInOut',
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+          },
+          exitState: {
+            clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
+          },
+        }}>
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
