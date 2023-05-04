eea4edc07bf9b9e87f9887ba6c9b2047c6ae8b21
Prashant <prashantrahul141@protonmail.com>
2023-05-04 15:03:37 +0530

    add: fonts and theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       colors: {
         'primary-theme': {
           bg: '#0a192f',
+          'cyan-100': '#1df2dd',
+          'cyan-200': '#21a6a6',
+          'white-100': '#ccd6f6',
+          'white-200': '#8892b0',
         },
+      },
+      fontFamily: {
+        inconsolata: 'Inconsolata, monospace',
+        'archivo-narrow': 'Archivo Narrow, sans-serif',
       },
     },
   },
