84b49550a83817f42708d6aec7d65f229cf40741
Prashant <prashantrahul141@protonmail.com>
2023-05-04 16:04:20 +0530

    add: navigationOPtions

---
+++ utils/constantTSX.tsx
@@ -0,0 +1,7 @@
+const navigationOptions: Array<{ text: string; link: string }> = [
+  { text: 'Home', link: '/home/' },
+  { text: 'Blogs', link: '/home/blogs' },
+  { text: 'Gallery', link: '/home/gallery' },
+];
+
+export { navigationOptions };
