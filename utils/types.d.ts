2d3cb5caf37afe79788f3de7fdfa0c1f5bf952a2
Prashant <prashantrahul141@protonmail.com>
2023-05-06 03:30:26 +0530

    add: TRepo, graphqlResponse

--- utils/types.d.ts
+++ utils/types.d.ts
@@ -1,3 +1,39 @@
 type TNavigationOptionsID = 0 | 1 | 2;
 
-export { TNavigationOptionsID };
+interface TRepo {
+  node: {
+    name: string;
+    repositoryTopics: {
+      edges: Array<{
+        node: {
+          id: string;
+          topic: {
+            id: string;
+            name: string;
+          };
+        };
+      }>;
+    };
+    primaryLanguage: {
+      id: string;
+      name: string;
+    };
+    description: string;
+    url: string;
+    stargazerCount: number;
+    owner: {
+      login: string;
+    };
+  };
+}
+
+interface graphqlResponse {
+  user: {
+    pinnedItems: {
+      totalCount: number;
+      edges: Array<TRepo>;
+    };
+  };
+}
+
+export { TNavigationOptionsID, graphqlResponse, TRepo };
