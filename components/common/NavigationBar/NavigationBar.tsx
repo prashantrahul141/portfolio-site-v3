970b1961b4e96864b950e17d53859f3d33649acf
Prashant <prashantrahul141@protonmail.com>
2023-05-05 18:51:05 +0530

    update: fixing nav bar position

--- components/common/NavigationBar/NavigationBar.tsx
+++ components/common/NavigationBar/NavigationBar.tsx
@@ -21,13 +21,13 @@ const NavigationBar: FC<{ defaultValue: TNavigationOptionsID }> = ({
   }, []);
 
   return (
-    <>
+    <div className='fixed left-0 top-0 z-10 w-screen'>
       {isDesktop ? (
         <NavigationBarLarge defaultValue={defaultValue} />
       ) : (
         <NavigationBarMobile defaultValue={defaultValue} />
       )}
-    </>
+    </div>
   );
 };
 export default NavigationBar;
