885bbf80e73fc4467a5c204665a4942af5b9a33c
Prashant <prashantrahul141@protonmail.com>
2023-05-06 03:31:34 +0530

    add: env check

---
+++ env.mjs
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const schema = z.object({
+  GITHUB_GRAPHQL_API_KEY: z.string(),
+});
+
+const expectedEnv = {
+  GITHUB_GRAPHQL_API_KEY: process.env.GITHUB_GRAPHQL_API_KEY,
+};
+
+const _env = schema.safeParse(expectedEnv);
+
+if (!_env.success) {
+  console.error('Invalid env vars.', _env.error);
+  throw new Error('Invalid env vars.');
+}
+
+export const env = { ..._env.data };
