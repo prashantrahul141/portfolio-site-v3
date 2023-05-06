67a3fa906a5b0baf07523706680cc60af3d2133b
Prashant <prashantrahul141@protonmail.com>
2023-05-06 17:25:45 +0530

    add: repo view

--- components/home/ProjectsInfo.tsx
+++ components/home/ProjectsInfo.tsx
@@ -1,23 +1,67 @@
 import type { FC } from 'react';
 import { motion } from 'framer-motion';
 import type { TRepo, graphqlResponse } from '@/utils/types';
+import { OutlineGHRepo, OutlineStar } from '../common/icons';
+import Link from 'next/link';
 
-const Repo: FC<{ repo: TRepo }> = ({ repo }) => {
+const Repo: FC<{ repo: TRepo; index: number }> = ({ repo, index }) => {
+  const currentRepo = repo.node;
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0 }}
-      whileInView={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, y: 25 }}
+      whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
-      transition={{ duration: 1 }}></motion.div>
+      transition={{ duration: 1, delay: index / 10 }}
+      className='group min-w-full max-w-[30rem] rounded-md bg-primary-theme-bg-50 p-7 transition-colors duration-500 hover:bg-primary-theme-bg-40'>
+      <Link href={currentRepo.url} passHref={true} target='_blank'>
+        <div>
+          <h1 className='flex items-center gap-2'>
+            <span className='text-sm text-primary-theme-repo-100'>
+              <OutlineGHRepo></OutlineGHRepo>
+            </span>
+            <span className='font-inconsolata text-base capitalize tracking-wide text-primary-theme-cyan-100 transition-colors duration-500 group-hover:text-primary-theme-cyan-100 sm:text-primary-theme-cyan-100/90'>
+              {currentRepo.name}
+            </span>
+          </h1>
+        </div>
+
+        <div className='h-full pb-8 pl-1 pt-[.3rem]'>
+          <p className='font-sans text-sm text-primary-theme-white-100 transition-colors duration-500 group-hover:text-primary-theme-white-100 sm:text-primary-theme-white-100/70'>
+            {currentRepo.description}
+          </p>
+        </div>
+
+        <div className='flex -translate-y-8 flex-col justify-end pl-1'>
+          <span className='flex items-center gap-5 font-inconsolata text-primary-theme-white-100 transition-colors duration-500 group-hover:text-primary-theme-white-100 sm:text-primary-theme-white-100/70'>
+            <span className='flex items-center gap-1 text-sm '>
+              <OutlineStar></OutlineStar>
+              <span>{currentRepo.stargazerCount}</span>
+            </span>
+            <span className='text-sm tracking-wide'>
+              {currentRepo.primaryLanguage.name}
+            </span>
+          </span>
+        </div>
+      </Link>
+    </motion.div>
   );
 };
 
 const ProjectsInfo: FC<{ ghResponse: graphqlResponse }> = ({ ghResponse }) => {
   return (
-    <div className='h-screen text-2xl text-white'>
-      {ghResponse.user.pinnedItems.edges.map((repo, index) => {
-        return <Repo key={index} repo={repo}></Repo>;
-      })}
+    <div className='flex min-h-screen items-center justify-center text-white'>
+      <div className='w-full max-w-5xl px-8'>
+        <div className='mb-12 '>
+          <span className=' font-archivo-narrow text-3xl text-primary-theme-white-100 sm:text-6xl'>
+            Projects I&apos;ve built
+          </span>
+        </div>
+        <div className='grid w-full gap-2 sm:grid-cols-2 '>
+          {ghResponse.user.pinnedItems.edges.map((repo, index) => {
+            return <Repo key={index} repo={repo} index={index}></Repo>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
