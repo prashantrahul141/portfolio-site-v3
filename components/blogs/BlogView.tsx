ad98581023ba94f2a615979aa6bae127b7c2c7f8
Prashant <prashantrahul141@protonmail.com>
2023-05-07 21:15:58 +0530

    add: blog view component

--- components/blogs/BlogView.tsx
+++ components/blogs/BlogView.tsx
@@ -1,7 +1,37 @@
+import { months } from '@/utils/constants';
+import type { TBlog } from '@/utils/types';
 import type { FC } from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
-const BlogView: FC = () => {
-  return <></>;
+const BlogView: FC<{ blog: TBlog & { fileContent: string } }> = ({ blog }) => {
+  const date = new Date(blog.Date);
+  const dateString = `${
+    months[date.getDay()]
+  } ${date.getMonth()}, ${date.getFullYear()}`;
+
+  return (
+    <div className=''>
+      <div className='mb-6 flex w-full flex-col items-center justify-center'>
+        <span className='px-1 font-archivo-narrow text-3xl tracking-wide text-primary-theme-white-50 sm:px-6'>
+          {blog.Title}
+          <div className='my-2 pl-[.12rem] font-inconsolata text-sm leading-none text-primary-theme-white-200'>
+            {dateString}
+          </div>
+        </span>
+      </div>
+
+      <div className='px-2'>
+        <ReactMarkdown
+          rehypePlugins={[rehypeRaw]}
+          remarkPlugins={[remarkGfm]}
+          components={{}}>
+          {blog.fileContent}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default BlogView;
