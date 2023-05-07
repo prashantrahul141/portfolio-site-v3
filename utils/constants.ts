4d53d087ef9f7caf3856ab59984566efc737ee18
Prashant <prashantrahul141@protonmail.com>
2023-05-07 17:19:32 +0530

    add: months to constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,15 @@
 export const GH_USERNAME = 'prashantrahul141';
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
