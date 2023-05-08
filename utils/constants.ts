bd759d69d636f2ca763d255edd66ecb6d5996a75
Prashant <prashantrahul141@protonmail.com>
2023-05-08 19:50:05 +0530

    moving query to constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -13,3 +13,42 @@ export const months = [
   'November',
   'December',
 ];
+
+export const GRAPH_QL_QUERY = `
+{
+  user(login: "prashantrahul141") {
+    pinnedItems(first: 6, types: [REPOSITORY]) {
+      totalCount
+      edges {
+        node {
+          ... on Repository {
+            name
+            repositoryTopics(first: 3) {
+              edges {
+                node {
+                  id
+                  topic {
+                    id
+                    name
+                  }
+                }
+              }
+            }
+            primaryLanguage {
+              id
+              name
+              color
+            }
+            description
+            url
+            stargazerCount
+            owner {
+              login
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
