6354378df0dca4283f6096a9cc5343e269c8d206
Prashant <prashantrahul141@protonmail.com>
2023-05-09 14:38:14 +0530

    add: more content to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,45 @@
 import HeadComponent from '@/components/common/HeadComponent';
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const FourZeroFour: NextPage = () => {
+  const [pathName, setPathName] = useState<string>('');
+
+  useEffect(() => {
+    const _path =
+      window.location.pathname.length > 15
+        ? window.location.pathname.slice(0, 15) + '...'
+        : window.location.pathname;
+
+    setPathName(_path.slice(1, _path.length));
+  }, []);
+
   return (
     <>
       <HeadComponent pageTitle='404'></HeadComponent>
-      <div className='text-2xl text-white'>THIS IS 404</div>;
+      <div className='flex h-screen w-full flex-col items-center justify-center gap-12'>
+        <div className='font-archivo-narrow text-3xl tracking-wide text-primary-theme-white-50 sm:text-4xl md:text-5xl'>
+          404 - Page not found.
+        </div>
+        <div className='font-archivo-narrow text-lg tracking-wide sm:text-xl md:text-2xl'>
+          <Link href={'/home'}>
+            <span className='text-primary-theme-white-200'>
+              prashantrahul.com/
+            </span>
+          </Link>
+          <span className='text-primary-theme-white-50'>{pathName}</span>
+          <span className='text-primary-theme-white-50'>&nbsp;!? 🤨</span>
+        </div>
+        <div className='font-inconsolata text-2xl tracking-wide text-primary-theme-cyan-100'>
+          <Link href={'/home'}>
+            <button className='animate-bounce-slow rounded border border-primary-theme-white-200 px-2 py-1  font-inconsolata text-xl tracking-wide text-primary-theme-white-100/90 transition-all duration-500 ease-in-out hover:border-primary-theme-white-100 hover:text-primary-theme-white-50'>
+              Home
+            </button>
+          </Link>
+        </div>
+      </div>
+      ;
     </>
   );
 };
