79a5e8f4a1cec3f18e40650e63703c14df4ec991
Prashant <prashantrahul141@protonmail.com>
2023-05-09 16:16:17 +0530

    updating og tags

--- components/common/HeadComponent.tsx
+++ components/common/HeadComponent.tsx
@@ -15,34 +15,33 @@ const HeadComponent: FC<{ pageTitle?: string }> = ({ pageTitle }) => {
         content='width=device-width, user-scalable=no'></meta>
       <meta name='darkreader-lock' />
 
-      <meta name='title' charSet='UTF-8' content='Prashant Rahul' />
       <meta
-        name='description'
-        charSet='UTF-8'
-        content='My portfolio website.'
+        name='twitter:image:src'
+        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
       />
+      <meta name='twitter:site' content='@prashantrahul' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='Prashant Rahul' />
+      <meta name='twitter:description' content='My portfolio website.' />
+      <meta property='twitter:domain' content='prashantrahul.com' />
+      <meta property='twitter:url' content='https://prashantrahul.com' />
 
-      <meta content='Prashant Rahul' property='og:title' />
-      <meta content='My portfolio website.' property='og:description' />
-      <meta content='https://prashantrahul.com' property='og:url' />
       <meta
         content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
         property='og:image'
       />
-      <meta content='#0c1d37' data-react-helmet='true' name='theme-color' />
+      <meta property='og:image:alt' content='My Portfolio Website.' />
+      <meta property='og:site_name' content='prashantrahul' />
+      <meta property='og:type' content='object' />
+      <meta property='og:title' charSet='UTF-8' content='Prashant Rahul' />
+      <meta
+        property='og:description'
+        charSet='UTF-8'
+        content='My portfolio website.'
+      />
+      <meta property='og:url' content='https://prashantrahul.com' />
 
-      <meta
-        name='twitter:card'
-        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
-      />
-      <meta property='twitter:domain' content='prashantrahul.com' />
-      <meta property='twitter:url' content='https://prashantrahul.com' />
-      <meta name='twitter:title' content='Prashant Rahul' />
-      <meta name='twitter:description' content='My portfolio website.' />
-      <meta
-        name='twitter:image'
-        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
-      />
+      <meta content='#0c1d37' data-react-helmet='true' name='theme-color' />
     </Head>
   );
 };
