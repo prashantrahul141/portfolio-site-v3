465443f3b50080bbf76b232be68ed63516041b37
Prashant <prashantrahul141@protonmail.com>
2023-05-09 15:32:51 +0530

    add: og tags to head component

--- components/common/HeadComponent.tsx
+++ components/common/HeadComponent.tsx
@@ -14,6 +14,35 @@ const HeadComponent: FC<{ pageTitle?: string }> = ({ pageTitle }) => {
         name='viewport'
         content='width=device-width, user-scalable=no'></meta>
       <meta name='darkreader-lock' />
+
+      <meta name='title' charSet='UTF-8' content='Prashant Rahul' />
+      <meta
+        name='description'
+        charSet='UTF-8'
+        content='My portfolio website.'
+      />
+
+      <meta content='Prashant Rahul' property='og:title' />
+      <meta content='My portfolio website.' property='og:description' />
+      <meta content='https://prashantrahul.com' property='og:url' />
+      <meta
+        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
+        property='og:image'
+      />
+      <meta content='#0c1d37' data-react-helmet='true' name='theme-color' />
+
+      <meta
+        name='twitter:card'
+        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
+      />
+      <meta property='twitter:domain' content='prashantrahul.com' />
+      <meta property='twitter:url' content='https://prashantrahul.com' />
+      <meta name='twitter:title' content='Prashant Rahul' />
+      <meta name='twitter:description' content='My portfolio website.' />
+      <meta
+        name='twitter:image'
+        content='https://raw.githubusercontent.com/prashantrahul141/prashantrahul141.github.io/main/public/meta/demo.png'
+      />
     </Head>
   );
 };
