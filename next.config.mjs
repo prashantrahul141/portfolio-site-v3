fe1f377b8b8d93af786811eb2ba45ca770761d18
Prashant <prashantrahul141@protonmail.com>
2023-05-13 20:24:51 +0530

    minify and romove image from next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,8 @@ await import('./env.mjs');
 
 /** @type {import("next").NextConfig} */
 const config = {
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   output: 'export',
 };
 export default config;
