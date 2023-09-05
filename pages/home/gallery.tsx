9a40c8d75b4c499be560b56ffce03fff00650433
Prashant <prashantrahul141@protonmail.com>
2023-09-06 02:30:22 +0530

    remove: full image capability

--- pages/home/gallery.tsx
+++ pages/home/gallery.tsx
@@ -3,12 +3,8 @@ import NavigationBar from '@/components/common/NavigationBar/NavigationBar';
 import GalleryImageList from '@/components/gallery/GalleryImageList';
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
 import path from 'path';
-import { motion } from 'framer-motion';
 import fsPromises from 'fs/promises';
-import FullImageView from '@/components/gallery/FullImageView';
-import { useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
-import { useHashRouteToggle } from '@/hooks/HashRouteToggle';
 
 export const getStaticProps: GetStaticProps<{
   filesPath: Array<{ original: string }>;
@@ -27,38 +23,14 @@ export const getStaticProps: GetStaticProps<{
 const Gallery: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   filesPath,
 }) => {
-  const [fullImage, setFullImage] = useState<string>(
-    filesPath[0].original || '1'
-  );
-  const [showFullImage, setShowFullImage] = useHashRouteToggle('#image');
-
   return (
     <>
       <HeadComponent pageTitle='Gallery'></HeadComponent>
       <div className='min-w-screen flex min-h-screen items-start justify-center bg-primary-theme-bg pt-24 sm:pt-32'>
         <NavigationBar defaultValue={2} />
-        <GalleryImageList
-          filesPath={filesPath}
-          callBackFn={(target: string) => {
-            setFullImage(target);
-            setShowFullImage(true);
-          }}></GalleryImageList>
-        <AnimatePresence>
-          {showFullImage && <FullImageView image={fullImage}></FullImageView>}
-        </AnimatePresence>
-        <AnimatePresence>
-          {showFullImage && (
-            <motion.div
-              className='fixed left-0 top-0 z-10 h-screen w-screen bg-primary-theme-bg/80 backdrop-blur-md'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              onClick={() => {
-                setShowFullImage(false);
-              }}
-              transition={{ duration: 0.2, ease: 'easeInOut' }}></motion.div>
-          )}
-        </AnimatePresence>
+        <GalleryImageList filesPath={filesPath}></GalleryImageList>
+        <AnimatePresence></AnimatePresence>
+        <AnimatePresence></AnimatePresence>
       </div>
     </>
   );
