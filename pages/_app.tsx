5ee66f7d224c1e67b304dcf94fa1254862600374
Prashant <prashantrahul141@protonmail.com>
2024-02-24 23:54:05 +0530

    "temprorily" disable CustomCursor because it looks janky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import CustomCursor from '@/components/common/customCursor';
-import useIsMobile from '@/hooks/useIsMobile';
+// import CustomCursor from '@/components/common/customCursor';
+// import useIsMobile from '@/hooks/useIsMobile';
 import BackgroundBlob from '@/components/common/backgroundBlob';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isMobile = useIsMobile();
+  // const isMobile = useIsMobile();
 
   return (
     <>
       <div className='fixed -z-10 h-screen w-screen bg-primary-theme-bg'></div>
       <Component {...pageProps} />
-      {!isMobile && <CustomCursor key={'customCursor'}></CustomCursor>}
+      {/* {!isMobile && <CustomCursor key={'customCursor'}></CustomCursor>} */}
       <BackgroundBlob></BackgroundBlob>
     </>
   );
