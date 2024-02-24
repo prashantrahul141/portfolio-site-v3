561b1a7fcec58e3e9c73d09010ccfe858f016d48
Prashant <prashantrahul141@protonmail.com>
2024-02-24 22:34:26 +0530

    fix warning , remove dead code

--- components/blogs/BlogView.tsx
+++ components/blogs/BlogView.tsx
@@ -1,29 +1,13 @@
 // need to disable this because we will be using react-markdown renderer.
 /* eslint-disable react/no-children-prop */
 
-import { months } from '@/utils/constants';
 import type { TBlog } from '@/utils/types';
 import type { FC } from 'react';
 import Markdown from 'react-markdown';
-import Image from 'next/image';
 
 const BlogView: FC<{ blog: TBlog & { fileContent: string } }> = ({ blog }) => {
-  const date = new Date(blog.Date);
-  const dateString = `${
-    months[date.getDay()]
-  } ${date.getMonth()}, ${date.getFullYear()}`;
-
   return (
     <div className=''>
-      {/* <div className='mb-6 flex w-full flex-col items-center justify-center'>
-        <span className='px-1 font-archivo-narrow text-3xl tracking-wide text-primary-theme-white-50 sm:px-6'>
-          {blog.Title}
-          <div className='my-2 pl-[.12rem] font-inconsolata text-sm leading-none text-primary-theme-white-200'>
-            {dateString}
-          </div>
-        </span>
-      </div> */}
-
       <div className='items-center justify-center px-2'>
         <Markdown
           children={blog.fileContent}
