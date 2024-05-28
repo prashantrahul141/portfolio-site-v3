1a4067768e3eb9394abf94c81b445d652ac84b58
Prashant <prashantrahul141@protonmail.com>
2024-05-28 14:28:06 +0530

    other stuff required by skillinfo page

--- utils/constantTSX.tsx
+++ utils/constantTSX.tsx
@@ -1,3 +1,6 @@
+import { skillIcons } from '@/components/common/icons';
+import type { ReactElement } from 'react';
+
 const navigationOptions = [
   { id: 0, text: 'Home', link: '/' },
   { id: 1, text: 'Blogs', link: '/blogs' },
@@ -5,3 +8,105 @@ const navigationOptions = [
 ] as const;
 
 export { navigationOptions };
+
+export type TIndiSkill = { name: string; icon: ReactElement };
+export type TSkillGroup = { groupName: string; values: Array<TIndiSkill> };
+export type TSkills = Array<TSkillGroup>;
+
+export const skillsMap: TSkills = [
+  {
+    groupName: 'Languages',
+    values: [
+      {
+        name: 'Rust',
+        icon: <skillIcons.RustLangIcon></skillIcons.RustLangIcon>,
+      },
+      {
+        name: 'Typescript',
+        icon: <skillIcons.TypescriptLangIcon></skillIcons.TypescriptLangIcon>,
+      },
+      {
+        name: 'Javascript',
+        icon: <skillIcons.JavascriptLangIcon></skillIcons.JavascriptLangIcon>,
+      },
+      {
+        name: 'Go',
+        icon: <skillIcons.GoLangIcon></skillIcons.GoLangIcon>,
+      },
+      {
+        name: 'C',
+        icon: <skillIcons.CLangIcon></skillIcons.CLangIcon>,
+      },
+      {
+        name: 'C++',
+        icon: <skillIcons.CPlusPlusLangIcon></skillIcons.CPlusPlusLangIcon>,
+      },
+      {
+        name: 'C#',
+        icon: <skillIcons.CSharpLangIcon></skillIcons.CSharpLangIcon>,
+      },
+
+      {
+        name: 'Python',
+        icon: <skillIcons.PythonLangIcon></skillIcons.PythonLangIcon>,
+      },
+      {
+        name: 'Lua',
+        icon: <skillIcons.LuaLangIcon></skillIcons.LuaLangIcon>,
+      },
+      {
+        name: 'Bash',
+        icon: <skillIcons.BashLangIcon></skillIcons.BashLangIcon>,
+      },
+      {
+        name: 'Kotlin',
+        icon: <skillIcons.KotlinLangIcon></skillIcons.KotlinLangIcon>,
+      },
+    ],
+  },
+  {
+    groupName: 'Technologies',
+    values: [
+      {
+        name: 'Next',
+        icon: <skillIcons.NextJsIcon></skillIcons.NextJsIcon>,
+      },
+      {
+        name: '.NET',
+        icon: <skillIcons.DotNetIcon></skillIcons.DotNetIcon>,
+      },
+      {
+        name: 'React',
+        icon: <skillIcons.ReactIcon></skillIcons.ReactIcon>,
+      },
+      {
+        name: 'OpenGL',
+        icon: <skillIcons.OpenGlIcon></skillIcons.OpenGlIcon>,
+      },
+      {
+        name: 'Tailwindcss',
+        icon: <skillIcons.TailwindcssIcon></skillIcons.TailwindcssIcon>,
+      },
+      {
+        name: 'Git & Github',
+        icon: <skillIcons.GitIcon></skillIcons.GitIcon>,
+      },
+      {
+        name: 'Express',
+        icon: <skillIcons.ExpressIcon></skillIcons.ExpressIcon>,
+      },
+      {
+        name: 'SQL',
+        icon: <skillIcons.PostgresIcon></skillIcons.PostgresIcon>,
+      },
+      {
+        name: 'Threejs',
+        icon: <skillIcons.ThreejsIcon></skillIcons.ThreejsIcon>,
+      },
+      {
+        name: 'SocketIO',
+        icon: <skillIcons.SocketIOIcon></skillIcons.SocketIOIcon>,
+      },
+    ],
+  },
+] as const;
