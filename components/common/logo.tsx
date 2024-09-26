9bbec00956195fdf0f67d425140730de583d8a99
Prashant <prashantrahul141@protonmail.com>
2024-09-26 17:16:46 +0530

    fix: logo path in blog routes

--- components/common/logo.tsx
+++ components/common/logo.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 
 const LogoSvg: FC<{ variant: 'preloader' | 'page' }> = () => (
   <img
-    src='logo.avif'
+    src='/logo.avif'
     alt='logo icon for prashantrahul.com'
     className='scale-75 opacity-85'
   />
